84e579252cd488e2cc0ea804d2842f5f4bfecf80
Bastion Fennell <bastionfennell@thefrontside.net>
2015-05-03 22:58:14 -0500

    Remove length

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-Song.create!(name: "nightclub1", artist: "local band", length: 1.7, bpm: 472, dance: "nc")
-Song.create!(name: "polka1", artist: "slightly less local band", length: 5.7, bpm: 2, dance: "polka")
-Song.create!(name: "nightclub2", artist: "Tracy Atkins", length: 2.7, bpm: 95, dance: "nc")
+Song.create!(name: "nightclub1", artist: "local band", bpm: 472, dance: "nc", cpl_length: 0.14, proam_length: 0.7, cpl_phrasing: [1, 2, 3, 4, 5], proam_phrasing: [1, 2, 3, 4, 5])
+Song.create!(name: "polka1", artist: "slightly less local band", bpm: 2, dance: "polka", cpl_length: 39.2, proam_length: 1.82, cpl_phrasing: [2, 2, 3, 4, 5], proam_phrasing: [2, 2, 3, 4, 5])
+Song.create!(name: "nightclub2", artist: "Tracy Atkins", bpm: 95, dance: "nc", cpl_length: 2.14, proam_length: 1.7, cpl_phrasing: [2, 2, 3, 4, 5], proam_phrasing: [2, 2, 3, 4, 5])
 
 
 #Song: {name, artist, bpm, dance, cpl_length, proam_length, cpl_phrasing, proam_length}
--- ember-app/app/models/song.js
+++ ember-app/app/models/song.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr("string"),
   artist: DS.attr("string"),
-  length: DS.attr(),
   bpm: DS.attr(),
   dance: DS.attr("string"),
   cpl_length: DS.attr(),
